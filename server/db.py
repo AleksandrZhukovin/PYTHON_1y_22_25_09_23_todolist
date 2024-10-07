203517d677384ab7847dc2b07c2a858442d07ef8
AleksandrZhukovin <sashokzuk@gmail.com>
2024-10-07 19:28:51 +0300

    add(db): Add models to database

--- server/db.py
+++ server/db.py
@@ -1,12 +1,51 @@
-"""
-Створити 3 таблиці
-Користувач
-    id, username, password, email
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
-Проекти
-    id, name, user
 
-Задача
-    id, name, deadline (DateTime), priority (Integer), is_done (Boolean), project, user
+DATABASE_URL = "sqlite:///./app.db"
 
-"""
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    name = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    user = relationship("User", back_populates="projects")
+
+
+class Task(Base):
+    __tablename__ = "task"
+
+    name = Column(String, nullable=False)
+    priority = Column(Integer, nullable=False, unique=True)
+    deadline = Column(DateTime)
+    is_done = Column(Boolean, nullable=False, default=False)
+    project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    user = relationship("User", back_populates="tasks")
+    project = relationship("Project", back_populates="tasks")
