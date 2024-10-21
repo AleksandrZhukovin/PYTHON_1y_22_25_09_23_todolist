16f7e02f1f439e521a3ee74727ff0a359ae639b7
AleksandrZhukovin <sashokzuk@gmail.com>
2024-10-21 19:26:04 +0300

    add login

--- server/static/js/add-project.js
+++ server/static/js/add-project.js
@@ -7,8 +7,13 @@ $('#saveProject').click(function (){
             'name': $('#projectName').val(),
         },
         'success': function (response){
+            let newProjectHTML = `<div class="card">
+                                           <h5 class="card-header">${$('#projectName').val()}</h5>
+                                           <div class="card-body">
+                                           </div>
+                                         </div>`
             let projectsDiv = document.getElementById('projects');
-            projectsDiv.innerHTML = `<h3>${$('#projectName').val()}</h3>` + projectsDiv.innerHTML;
+            projectsDiv.innerHTML = newProjectHTML + projectsDiv.innerHTML;
             $('#projectName').val('');
             const projectModal = new bootstrap.Modal('#projectModal');
             projectModal.hide();
--- server/views.py
+++ server/views.py
@@ -1,8 +1,8 @@
 from .config import *
-from .db import get_db, Project
+from .db import get_db, Project, User
 
 from fastapi import Request, Depends, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
 
@@ -11,16 +11,43 @@ __all__ = (
 )
 
 
+def login_required(func):
+    def wrap(request: Request, *args, **kwargs):
+        if not request.session.get('is_auth', False):
+            return RedirectResponse('/login', status_code=303)
+        return func(request, *args, **kwargs)
+    return wrap
+
+
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request, db: Session = Depends(get_db)):
     projects = db.query(Project).all()
     return templates.TemplateResponse('index.html', {'projects': projects, 'request': request})
 
 
+@login_required
 @app.post('/add-project')
-def add_project(name = Form(), db: Session = Depends(get_db)):
-    project = Project(name=name, user_id=1)
+def add_project(request: Request, name = Form(), db: Session = Depends(get_db)):
+    project = Project(name=name, user_id=request.session['user_id'])
     db.add(project)
     db.commit()
     db.refresh(project)
     return {}
+
+
+@app.get('/login', response_class=HTMLResponse)
+def login(request: Request, after_fail: bool = False):
+    context = {'request': request}
+    if after_fail:
+        context['message'] = 'Username or password is incorrect'
+    return templates.TemplateResponse('login.html', context)
+
+
+@app.post('/login')
+def login(request: Request, name: str = Form(), password: str = Form(), db: Session = Depends(get_db)):
+    user = db.query(User).filter_by(name=name).first()
+    if user is None or user.password != password:
+        return RedirectResponse('/login?after_fail=True', status_code=303)
+    request.session['user_id'] = user.id
+    request.session['is_auth'] = True
+    return RedirectResponse('/', status_code=303)
